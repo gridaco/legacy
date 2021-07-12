67eee49193f250ff81f325fced55f788fb5e1087
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-12 18:18:18 +0900

    pwa wip

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+// const withPWA = require("next-pwa");
 const withTM = require("next-transpile-modules")(
   [
     "@design-sdk/figma-remote",
@@ -13,17 +14,6 @@ const withTM = require("next-transpile-modules")(
     debug: true,
   }
 );
-
-const FIREBASE_ENV_VARS = {
-  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUKET: process.env.FIREBASE_STORAGE_BUKET,
-  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-};
 
 const LOCAL_DEVELOPMENT_ENV_VARS = {
   FIGMA_PERSONAL_ACCESS_TOKEN: process.env.FIGMA_PERSONAL_ACCESS_TOKEN,
@@ -69,7 +59,6 @@ module.exports = withTM({
     return config;
   },
   env: {
-    ...FIREBASE_ENV_VARS,
     ...LOCAL_DEVELOPMENT_ENV_VARS,
   },
 
