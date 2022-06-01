b7638cea1e3e978c48b0df7735415750d245003f
softmarshmallow <woojoo@softmarshmallow.com>
2022-06-02 06:27:14 +0900

    add global pipeline

--- services/workspace-service/src/main.ts
+++ services/workspace-service/src/main.ts
@@ -1,9 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.listen(3000, () => console.log('Microservice is listening'));
+  app.useGlobalPipes(new ValidationPipe());
+  app.listen(4401, () => console.log("Microservice is listening"));
 }
 bootstrap();
